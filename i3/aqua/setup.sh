f6de95a532a250ba4ce88f1298ffb1985350ad82
abanoub <90291414+abanoub-R@users.noreply.github.com>
2021-10-09 18:14:16 -0400

    Added installer

---
+++ i3/aqua/setup.sh
@@ -0,0 +1,7 @@
+# Auto setup script for 'aqua' i3 theme by abanoub-R on github
+
+# move config into ~/config/i3
+mv config ~/.config/i3/
+
+# move i3status.conf into /etc
+sudo mv i3status.conf /etc/
