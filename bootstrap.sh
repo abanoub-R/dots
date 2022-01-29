e9bfcf87fbdbe9e25b1d3ec1981125034f37edc4
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-01-29 10:45:25 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,19 @@
-# THIS IS NOT TESTED! USE AT YOUR OWN RISK!
-pacman -S firefox steam discord dunst vim maim rxvt-unicode git alsa-utils lxappearance pamixer
+# THIS SCRIPT IS I
+pacman -S systemd firefox steam discord dunst vim maim rxvt-unicode git alsa-utils lxappearance pamixer terminus-font
+
+# setup
+cd dwm
+sudo make clean install
+
+cd dwmblocks
+sudo make clean install
+
+cd ..
+cd system
+
+# move configs
+mv .Xresources ~
+mv .bashrc ~
+mv .profile ~
+mv .vimrc ~
+
