769c56a0c7975ea8d03efa66520e99cb0901095f
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-01-30 11:52:10 -0500

    added more functionality
    automatically moves configs and sets up everything in the repo.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
-# THIS SCRIPT IS I
-pacman -S systemd firefox steam discord dunst vim maim rxvt-unicode git alsa-utils lxappearance pamixer terminus-font
+clear
+echo "THIS SCRIPT IS INTENDED TO BE USED ON FRESH INSTALLS OF ARCH / ARCH BASED DISTROS, DO NOT RUN OTHERWISE."
+echo "THIS SCRIPT WILL ONLY WORK IF RAN AS SUDO"
 
-# setup
+echo "Installing apps..."
+pacman -S firefox steam discord dunst vim maim rxvt-unicode git alsa-utils lxappearance pamixer terminus-font
+
+echo "Setting up dwm..."
 cd dwm
 sudo make clean install
 
@@ -11,9 +15,19 @@ sudo make clean install
 cd ..
 cd system
 
-# move configs
+echo "Moving configs..."
 mv .Xresources ~
 mv .bashrc ~
 mv .profile ~
 mv .vimrc ~
 
+echo "Setting up scripts..."
+cd scripts
+echo "Marking as executable..."
+chmod +x down-vol
+chmod +x up-vol
+chmod +x mute-vol
+chmod +x get-cpu
+mv * /bin/
+
+echo "Setup is complete."
