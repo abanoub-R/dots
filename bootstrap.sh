e5e5da7828cbf90f6de4b4d9def910c341f50143
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-02-14 19:49:34 -0500

    removed unneccessary pkgs
    removed alsa-utils and git

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,15 +8,14 @@ echo "waiting for 5 seconds.."
 sleep 5
 
 echo "Initializing..."
-# correcting home address
-home=/home/$(whoami)
-# updating ps1
-echo 'export PS1="\[\033[38;5;27m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\]\[\033[38;5;27m\][\[$(tput sgr0)\]\[\033[38;5;33m\]\u\[$(tput s    gr0)\]\[\033[38;5;27m\]]\[$(tput sgr0)\] : \[$(tput sgr0)\]\[\033[38;5;241m\]\w\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;27m    \]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"'
+home=/home/$(whoami) # correcting home address
+workdir=$(pwd) && cd workdir # going to repo location to ensure proper functionality
+echo 'export PS1="\[\033[38;5;27m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\]\[\033[38;5;27m\][\[$(tput sgr0)\]\[\033[38;5;33m\]\u\[$(tput s    gr0)\]\[\033[38;5;27m\]]\[$(tput sgr0)\] : \[$(tput sgr0)\]\[\033[38;5;241m\]\w\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;27m    \]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"' # updating ps1
 
 
 echo "Installing apps..."
 
-pacman -S xclip xorg-server xorg-xrandr xorg-xinit pulseaudio firefox steam discord dunst vim maim rxvt-unicode git alsa-utils lxappearance pamixer terminus-font nitrogen
+pacman -S xclip xorg-server xorg-xrandr xorg-xinit pulseaudio firefox steam discord dunst vim maim rxvt-unicode lxappearance pamixer terminus-font nitrogen
 
 echo "Setting up dwm..."
 cd dwm
@@ -25,20 +24,21 @@ sudo make clean install
 cd dwmblocks
 sudo make clean install
 
-cd ..;cd ..
+cd .. && cd ..
 cd system
 
 echo "Moving configs..."
-mv .Xresources $(home)
-mv .bashrc $(home)
-mv .profile $(home)
-mv .vimrc $(home)
-mv dunstrc $(home)/.config/dunst/
+mv .Xresources $home
+mv .bashrc $home
+mv .profile $home
+mv .vimrc $home
+mv dunstrc $home/.config/dunst/
 
 echo "Setting up scripts..."
 cd scripts
-echo "Marking as executable..."
-chmod +x *
-mv * /bin/
+
+echo "Marking scripts as executable..."
+chmod +x * # marking all contents as exec.
+mv * /bin/ # moving all contents to bin
 
 echo "Setup is complete, you may remove all installation files BUT the dwm directory as it contains source code you may wish to edit"
