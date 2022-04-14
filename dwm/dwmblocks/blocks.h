73add9dc7cd6b6d77eb074ed60e6dfaef04805ff
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-04-14 19:42:35 -0400

    Add files via upload

--- dwm/dwmblocks/blocks.h
+++ dwm/dwmblocks/blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" CPU : ",	"get-cpu &",		2,                      0},
+	{" CPU : ",	"get-cpu",		2,                      0},
 
-	{"RAM : ", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		1,			0},
+	{"RAM : ", 	"get-ram",		1,			0},
         
-	{"♪ : ",	"awk -F'[][]' '/Left:/ { print $2 }' <(amixer sget Master)",	0,			10},
+	{"♪ : ",	"get-vol",		0,			10},
 
-	{"", "date '+%m/%d/%Y %I:%M %p'",	1,			0},
+	{"", 		"get-date",		1,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
