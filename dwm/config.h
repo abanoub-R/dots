fef518cc8ad88782b7c32c96881526631bc13db5
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-04-16 09:09:01 -0400

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include "colors.h"
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */ 
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -7,14 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
 static const char dmenufont[]       = "terminus:size=12";
-static const char col_white[]       = "#ffffff";
-static const char col_black[]       = "#101010";
-static const char col_gray1[]       = "#0f0f0f";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_blue1[] 	    = "#1071b2";
-static const char col_blue2[]	    = "#224488";
+
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
