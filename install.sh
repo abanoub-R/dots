179303a07ee1fb49800edfb88c1c05a0df7f3bb6
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-07-13 14:32:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this is unfinished
 
 # super user options
 echo "!!! DO NOT RUN AS ROOT !!!"
@@ -27,19 +28,17 @@ echo "- pacman"
 echo "- portage"
 
 read pkg   # system package manager
-syntax=""  # uses ^ to return the right syntax
-           
-           # generally this will give options
-           # to sync / update the package
-           # managers because why not
+
+echo "-- installing dependencies"
 
 case $pkg in
   "xbps") echo "-- continuing with xbps options"
-          syntax="xbps-install -Su "
+          echo "-- note: this will take a while especially on fresh installs as not only are these packages and their dependencies being installed, but it will also have to sync everything else"
+          $method xbps-install -Su alsa-firmware alsa-utils base-system dejavu-fonts-ttf dmenu elogind ffmpeg firefox-esr gcc libX11-devel libXft-devel libavcodec libdrm-32bit-2.4.110_1 libgcc-32bit libglapi-32bit libglvnd-32bit libstdc++-32bit libva-glx-32bit linux-firmwarelxappearance maim make mesa-dri mesa-dri-32bit mesa-vaapi neofetch nitrogen pulseaudio rxvt-unicode steam terminus-font ttf-ubuntu-font-family vim void-repo-multilib void-repo-multilib void-repo-nonfree-9_6 xclip xorg-minimal xrandr xrdb xz
   ;;
   
   "pacman") echo "-- continuing with pacman options"
-            syntax="pacman -Syu "
+            $method pacman -Syu alsa-utils xorg-server
   ;;
   
   "portage") echo "--continuing with portage options"
