80ebf491a098e4261f079a2fec08917a3383fb36
abanoub <90291414+abanoub-R@users.noreply.github.com>
2022-10-04 18:13:11 -0400

    change colorscheme to tango

--- st/config.h
+++ st/config.h
@@ -95,25 +95,25 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+        /* 8 normal colors */
+        "#000000",
+        "#cc0000",
+        "#4e9a06",
+        "#c4a000",
+        "#3465a4",
+        "#75507b",
+        "#06989a",
+        "#d3d7cf",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+        /* 8 bright colors */
+        "#555753",
+        "#ef2929",
+        "#8ae234",
+        "#fce94f",
+        "#729fcf",
+        "#ad7fa8",
+        "#34e2e2",
+        "#eeeeec",
 
 	[255] = 0,
 
